e711a4f9a5d1e274defe0ee165542a4fee048f87
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2022-03-01 11:41:03 -0800

    Added CSS

--- src/Weather.js
+++ src/Weather.js
@@ -11,33 +11,40 @@ export default function Weather() {
               type="search"
               placeholder="Enter your city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" />
+            <input type="submit" value="Search" className="button" />
           </div>
         </div>
       </form>
-      <div className="row city-date">
-        <h2>Los Angeles</h2>
-        <h3>Date | Month, Day | Hr:Min</h3>
-      </div>
 
-      <div class="row current-weather">
-        <div class="col">
-          <h1>20°</h1>
-          <img src="./images/01d_241.png" alt="sunny" className="Images" />
-        </div>
-        <span className="text-capitalize">sunny</span>
-      </div>
-
-      <div class="row justify-content-center high-low">
-        <div class="col-2">
-          <h3>H: 20°</h3>
+      <div className="container">
+        <div className="row city-date">
+          <h2>Los Angeles</h2>
+          <h3>Thursday | March, 1 | 10:54</h3>
         </div>
 
-        <div class="col-2">
-          <h3>L: 20°</h3>
+        <button className="unit">C</button>
+        <button className="unit">F</button>
+
+        <div class="row current-weather">
+          <div class="col">
+            <h1>20°</h1>
+            <img src="./images/01d_241.png" alt="sunny" className="Images" />
+          </div>
+          <span className="text-capitalize">sunny</span>
+        </div>
+
+        <div class="row justify-content-center high-low">
+          <div class="col-2">
+            <h3>H: 20°</h3>
+          </div>
+
+          <div class="col-2">
+            <h3>L: 20°</h3>
+          </div>
         </div>
       </div>
     </div>
